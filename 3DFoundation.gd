1662312e4e1f341087267a715d271b26e9840e5c
TheBogdichHD <nottoowellconstructed@gmail.com>
2024-10-24 20:57:32 +0300

    added addon and class Spatial

--- 3DFoundation.gd
+++ 3DFoundation.gd
@@ -1,26 +1,7 @@
 extends Node
 
-class Point:
-	var x: float
-	var y: float
-	
-	func _init(x:float, y:float):
-		self.x = x
-		self.y = y
-	
-	func minus(p: Point):
-		return Point.new(self.x - p.x, self.y - p.y)
 
-
-class Polygon:
-	var points: Array[Point]
-	
-	func _init() -> void:
-		points = []
-	func add(p: Point):
-		points.append(p)
-	func pop():
-		points.pop_back()
-	func clear():
-		points.clear()
-	
+class Spatial:
+	var points: Array[Vector3]
+	var edges: Array[Vector2i]
+	var faces #Array[Array[int]]
