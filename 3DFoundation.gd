c727be2f0a1b7800531e234450c1c1fe4e9b9819
Владислав Крылов <krylov@sfedu.ru>
2024-10-24 21:11:03 +0300

    added methods for 3DFoundation.gd

--- 3DFoundation.gd
+++ 3DFoundation.gd
@@ -1,7 +1,20 @@
+class_name F
 extends Node
 
 
 class Spatial:
 	var points: Array[Vector3]
 	var edges: Array[Vector2i]
-	var faces #Array[Array[int]]
+	#var faces #Array[Array[int]]
+	
+	func add_edge(p1: Vector3, p2: Vector3):
+		points.append(p1)
+		points.append(p2)
+		edges.append(Vector2i(points.size() - 2, points.size() - 1))
+	func pop_back():
+		edges.pop_back()
+		points.pop_back()
+		points.pop_back()
+	func clear():
+		points.clear()
+		edges.clear()
--- main.gd
+++ main.gd
@@ -1,4 +1,18 @@
 extends Control
 
+var edge_length = 100
 
-func
+func _ready() -> void:
+	var cube: F.Spatial = F.Spatial.new()
+	cube.add_edge(Vector3(100, 100, 10), Vector3(100 + edge_length, 100, 10))
+	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
+	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
+	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
+	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
+	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
+	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
+	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
+	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
+	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
+	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
+	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
