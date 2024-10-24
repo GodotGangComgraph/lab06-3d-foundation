030c2809763e13e40ddd777f6ef0a799202733dd
TheBogdichHD <nottoowellconstructed@gmail.com>
2024-10-24 22:24:08 +0300

    add cube and tetrahedron

--- 3DFoundation.gd
+++ 3DFoundation.gd
@@ -11,10 +11,52 @@ class Spatial:
 		points.append(p1)
 		points.append(p2)
 		edges.append(Vector2i(points.size() - 2, points.size() - 1))
-	func pop_back():
-		edges.pop_back()
-		points.pop_back()
-		points.pop_back()
+	
 	func clear():
 		points.clear()
 		edges.clear()
+
+
+class Cube extends Spatial:
+	func _init():
+		var l = 100
+		
+		points = [
+			Vector3(-l, -l, -l), Vector3(l, -l, -l), Vector3(l, l, -l), Vector3(-l, l, -l),
+			Vector3(-l, -l, l), Vector3(l, -l, l), Vector3(l, l, l), Vector3(-l, l, l)
+		]
+		
+		for i in range(points.size()):
+			points[i] += Vector3(200, 200, 200)
+		
+		var edge_pairs = [
+			Vector2i(0, 1), Vector2i(1, 2), Vector2i(2, 3), Vector2i(3, 0),
+			Vector2i(4, 5), Vector2i(5, 6), Vector2i(6, 7), Vector2i(7, 4),
+			Vector2i(0, 4), Vector2i(1, 5), Vector2i(2, 6), Vector2i(3, 7)
+		]
+		
+		for pair in edge_pairs:
+			add_edge(points[pair.x], points[pair.y])
+
+
+class Tetrahedron extends Spatial:
+	func _init():
+		var l = 100
+		
+		points = [
+			Vector3(l, l, l),
+			Vector3(-l, -l, l),
+			Vector3(-l, l, -l),
+			Vector3(l, -l, -l)
+		]
+		
+		for i in range(points.size()):
+			points[i] += Vector3(200, 200, 200)
+		
+		var edge_pairs = [
+			Vector2i(0, 1), Vector2i(0, 2), Vector2i(0, 3),
+			Vector2i(1, 2), Vector2i(1, 3), Vector2i(2, 3)
+		]
+		
+		for pair in edge_pairs:
+			add_edge(points[pair.x], points[pair.y])
--- main.gd
+++ main.gd
@@ -1,18 +1,49 @@
 extends Control
 
+
 var edge_length = 100
 
+var aspect_ratio = 1
+var near = 500.0
+var fov = 200.0
+
+var cube = F.Cube.new()
+var tetrahedron = F.Tetrahedron.new()
+
+# Perspective projection function
+func perspective_project(point: Vector3) -> Vector2:
+	var scale_ = near / (point.z + fov)
+	return Vector2(point.x * scale_ * aspect_ratio, point.y * scale_)
+
+
+## Perspective projection function
+#func perspective_project(point: Vector3) -> Vector2:
+	#var m = DenseMatrix.zero(4)
+	#m.set_element(0, 0, n)
+	#m.set_element(1, 1, n)
+	#m.set_element(2, 2, f+n)
+	#m.set_element(2, 3, -f*n)
+	#m.set_element(3, 2, 1)
+	#var v = DenseMatrix.from_packed_array([point.x, point.y, point.z, 1], 4, 1)
+	#var res = m.multiply_dense(v)
+	#
+	#return Vector2(res.get_element(0, 0), res.get_element(0, 1))
+
+
 func _ready() -> void:
-	var cube: F.Spatial = F.Spatial.new()
-	cube.add_edge(Vector3(100, 100, 10), Vector3(100 + edge_length, 100, 10))
-	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
-	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
-	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
-	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
-	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
-	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
-	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
-	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
-	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
-	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
-	cube.add_edge(Vector3(100, 100, 10), Vector3(100, 100, 10))
+	queue_redraw()
+
+
+func _draw():
+	# Drawing the cube in 2D space with perspective projection
+	for edge in tetrahedron.edges:
+		var p1 = tetrahedron.points[edge.x]
+		var p2 = tetrahedron.points[edge.y]
+		
+		# Apply perspective projection to 2D
+		var projected_p1 = perspective_project(p1)
+		var projected_p2 = perspective_project(p2)
+		
+		print(projected_p1, projected_p2)
+		# Draw the edges (replace with actual drawing code)
+		draw_line(projected_p1, projected_p2, Color.RED)
