c18826d33dea2980a97a3c32a41c282d8fb00b8c
Владислав Крылов <krylov@sfedu.ru>
2024-10-25 17:38:45 +0300

    update spatial and point, translation added

--- 3DFoundation.gd
+++ 3DFoundation.gd
@@ -1,13 +1,40 @@
-class_name F
-extends Node
+class_name F extends Node
 
+class AffineMatrices:
+
+	static func get_translation_matrix(tx: float, ty: float, tz: float) -> DenseMatrix:
+		var m = DenseMatrix.identity(4)
+		m.set_element(3, 0, tx)
+		m.set_element(3, 1, ty)
+		m.set_element(3, 2, tz)
+		return m
+
+class Point:
+	var x: float
+	var y: float
+	var z: float
+	func _init(_x: float, _y: float, _z: float) -> void:
+		x = _x
+		y = _y
+		z = _z
+		
+	func apply_matrix(matrix: DenseMatrix):
+		var v = get_vector()
+		var vnew = v.multiply_dense(matrix)
+		x = vnew.get_element(0, 0) 
+		y = vnew.get_element(0, 1) 
+		z = vnew.get_element(0, 2) 
+		pass
+		
+	func get_vector() -> DenseMatrix:
+		return DenseMatrix.from_packed_array([x, y, z, 1], 1, 4)
 
 class Spatial:
-	var points: Array[Vector3]
+	var points: Array[Point]
 	var edges: Array[Vector2i]
 	#var faces #Array[Array[int]]
 	
-	func add_edge(p1: Vector3, p2: Vector3):
+	func add_edge(p1: Point, p2: Point):
 		points.append(p1)
 		points.append(p2)
 		edges.append(Vector2i(points.size() - 2, points.size() - 1))
@@ -15,26 +42,30 @@ class Spatial:
 	func clear():
 		points.clear()
 		edges.clear()
-
-
+	
+	func apply_matrix(matrix: DenseMatrix):
+		for i in range(points.size()):
+			points[i].apply_matrix(matrix)
+		
+	func translate(tx, ty, tz):
+		var matrix: DenseMatrix = AffineMatrices.get_translation_matrix(tx, ty, tz)
+		apply_matrix(matrix)
+		
 class Cube extends Spatial:
 	func _init():
 		var l = 100
-		
 		points = [
-			Vector3(-l, -l, -l), Vector3(l, -l, -l), Vector3(l, l, -l), Vector3(-l, l, -l),
-			Vector3(-l, -l, l), Vector3(l, -l, l), Vector3(l, l, l), Vector3(-l, l, l)
+			Point.new(-l, -l, -l)	, Point.new(l, -l, -l),
+			Point.new(l, l, -l)		, Point.new(-l, l, -l),
+			Point.new(-l, -l, l)	, Point.new(l, -l, l), 
+			Point.new(l, l, l)		, Point.new(-l, l, l)
 		]
-		
-		for i in range(points.size()):
-			points[i] += Vector3(200, 200, 200)
-		
+		translate(200, 200, 200)
 		var edge_pairs = [
 			Vector2i(0, 1), Vector2i(1, 2), Vector2i(2, 3), Vector2i(3, 0),
 			Vector2i(4, 5), Vector2i(5, 6), Vector2i(6, 7), Vector2i(7, 4),
 			Vector2i(0, 4), Vector2i(1, 5), Vector2i(2, 6), Vector2i(3, 7)
 		]
-		
 		for pair in edge_pairs:
 			add_edge(points[pair.x], points[pair.y])
 
@@ -44,14 +75,13 @@ class Tetrahedron extends Spatial:
 		var l = 100
 		
 		points = [
-			Vector3(l, l, l),
-			Vector3(-l, -l, l),
-			Vector3(-l, l, -l),
-			Vector3(l, -l, -l)
+			Point.new(l, l, l),
+			Point.new(-l, -l, l),
+			Point.new(-l, l, -l),
+			Point.new(l, -l, -l)
 		]
 		
-		for i in range(points.size()):
-			points[i] += Vector3(200, 200, 200)
+		translate(200, 200, 200)
 		
 		var edge_pairs = [
 			Vector2i(0, 1), Vector2i(0, 2), Vector2i(0, 3),
--- main.gd
+++ main.gd
@@ -11,7 +11,7 @@ var cube = F.Cube.new()
 var tetrahedron = F.Tetrahedron.new()
 
 # Perspective projection function
-func perspective_project(point: Vector3) -> Vector2:
+func perspective_project(point: F.Point) -> Vector2:
 	var scale_ = near / (point.z + fov)
 	return Vector2(point.x * scale_ * aspect_ratio, point.y * scale_)
 
@@ -29,11 +29,17 @@ func perspective_project(point: Vector3) -> Vector2:
 	#
 	#return Vector2(res.get_element(0, 0), res.get_element(0, 1))
 
+var frame_count = 0
 
 func _ready() -> void:
+	Engine.max_fps = 20
+func _process(delta: float) -> void:
+	if (frame_count > 100):
+		return
+	frame_count += 1
+	tetrahedron.translate(0, 0, 1)
 	queue_redraw()
-
-
+	
 func _draw():
 	# Drawing the cube in 2D space with perspective projection
 	for edge in tetrahedron.edges:
@@ -44,6 +50,6 @@ func _draw():
 		var projected_p1 = perspective_project(p1)
 		var projected_p2 = perspective_project(p2)
 		
-		print(projected_p1, projected_p2)
+		#print(projected_p1, projected_p2)
 		# Draw the edges (replace with actual drawing code)
 		draw_line(projected_p1, projected_p2, Color.RED)
