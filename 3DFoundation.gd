d46246fdf9d4c89f33d4c1008f709dd137342994
Владислав Крылов <krylov@sfedu.ru>
2024-10-26 15:57:27 +0300

    axis added, translation reworked, perspective added

--- 3DFoundation.gd
+++ 3DFoundation.gd
@@ -1,4 +1,4 @@
-class_name F extends Node
+class_name F extends Control
 
 @onready var dx = $VBoxContainer/MarginContainer/VBoxContainer2/traslate/dx
 @onready var dy = $VBoxContainer/MarginContainer/VBoxContainer2/traslate/dy
@@ -14,6 +14,25 @@ class_name F extends Node
 
 
 class AffineMatrices:
+	
+	static func get_perspective_matrix(c: float) -> DenseMatrix:
+		var m = DenseMatrix.identity(4)
+		m.set_element(2, 2, 0)
+		m.set_element(2, 3, -1.0/c)
+		return m
+
+	static func get_axonometric_matrix(phi_deg: float, psi_deg: float) -> DenseMatrix:
+		var phi = deg_to_rad(phi_deg)
+		var psi = deg_to_rad(psi_deg)
+		var m = DenseMatrix.zero(4)
+		m.set_element(0, 0, cos(psi))
+		m.set_element(0, 1, sin(psi) * cos(phi))
+		m.set_element(1, 1, cos(phi))
+		m.set_element(2, 0, sin(psi))
+		m.set_element(2, 1, -sin(phi) * cos(psi))
+		m.set_element(3, 3, 1)
+		return m
+		
 
 	static func get_translation_matrix(tx: float, ty: float, tz: float) -> DenseMatrix:
 		var m = DenseMatrix.identity(4)
@@ -57,6 +76,7 @@ class Point:
 	var x: float
 	var y: float
 	var z: float
+	
 	func _init(_x: float, _y: float, _z: float) -> void:
 		x = _x
 		y = _y
@@ -65,13 +85,19 @@ class Point:
 	func apply_matrix(matrix: DenseMatrix):
 		var v = get_vector()
 		var vnew = v.multiply_dense(matrix)
-		x = vnew.get_element(0, 0) 
-		y = vnew.get_element(0, 1) 
-		z = vnew.get_element(0, 2) 
+		x = vnew.get_element(0, 0) / vnew.get_element(0, 3) 
+		y = vnew.get_element(0, 1) / vnew.get_element(0, 3)
+		z = vnew.get_element(0, 2) / vnew.get_element(0, 3)
 		pass
 		
 	func get_vector() -> DenseMatrix:
 		return DenseMatrix.from_packed_array([x, y, z, 1], 1, 4)
+	
+	func get_vec2d():
+		return Vector2(x, y)
+	
+	func get_vec3d():
+		return Vector3(x, y, z)
 
 class Spatial:
 	var points: Array[Point]
@@ -91,7 +117,7 @@ class Spatial:
 		for i in range(points.size()):
 			points[i].apply_matrix(matrix)
 		
-	func translate(tx, ty, tz):
+	func translate(tx: float, ty: float, tz: float):
 		var matrix: DenseMatrix = AffineMatrices.get_translation_matrix(tx, ty, tz)
 		apply_matrix(matrix)
 		
@@ -114,39 +140,58 @@ class Spatial:
 class Cube extends Spatial:
 	func _init():
 		var l = 100
-		points = [
+		var lst_points = [
 			Point.new(-l, -l, -l)	, Point.new(l, -l, -l),
 			Point.new(l, l, -l)		, Point.new(-l, l, -l),
 			Point.new(-l, -l, l)	, Point.new(l, -l, l), 
 			Point.new(l, l, l)		, Point.new(-l, l, l)
 		]
-		translate(200, 200, 200)
 		var edge_pairs = [
 			Vector2i(0, 1), Vector2i(1, 2), Vector2i(2, 3), Vector2i(3, 0),
 			Vector2i(4, 5), Vector2i(5, 6), Vector2i(6, 7), Vector2i(7, 4),
 			Vector2i(0, 4), Vector2i(1, 5), Vector2i(2, 6), Vector2i(3, 7)
 		]
 		for pair in edge_pairs:
-			add_edge(points[pair.x], points[pair.y])
+			add_edge(lst_points[pair.x], lst_points[pair.y])
 
 
 class Tetrahedron extends Spatial:
 	func _init():
 		var l = 100
 		
-		points = [
+		## THIS IS NOT POINTS FROM SPATIAL
+		var lst_points = [
 			Point.new(l, l, l),
 			Point.new(-l, -l, l),
 			Point.new(-l, l, -l),
 			Point.new(l, -l, -l)
 		]
 		
-		translate(200, 200, 200)
-		
+		## THIS IS NOT EDGES FROM SPATIAL
 		var edge_pairs = [
 			Vector2i(0, 1), Vector2i(0, 2), Vector2i(0, 3),
 			Vector2i(1, 2), Vector2i(1, 3), Vector2i(2, 3)
 		]
 		
 		for pair in edge_pairs:
-			add_edge(points[pair.x], points[pair.y])
+			add_edge(lst_points[pair.x], lst_points[pair.y])
+
+class Axis extends Spatial:
+	func _init():
+		var l = 100
+		
+		## THIS IS NOT POINTS FROM SPATIAL
+		var lst_points = [
+			Point.new(0, 0, 0),
+			Point.new(l, 0, 0),
+			Point.new(0, l, 0),
+			Point.new(0, 0, l),
+		]
+		
+		## THIS IS NOT EDGES FROM SPATIAL
+		var edge_pairs = [
+			Vector2i(0, 1), Vector2i(0, 2), Vector2i(0, 3),
+		]
+		
+		for pair in edge_pairs:
+			add_edge(lst_points[pair.x], lst_points[pair.y])	
--- main.gd
+++ main.gd
@@ -2,54 +2,59 @@ extends Control
 
 
 var edge_length = 100
-
-var aspect_ratio = 1
-var near = 500.0
-var fov = 200.0
-
 var cube = F.Cube.new()
 var tetrahedron = F.Tetrahedron.new()
-
-# Perspective projection function
-func perspective_project(point: F.Point) -> Vector2:
-	var scale_ = near / (point.z + fov)
-	return Vector2(point.x * scale_ * aspect_ratio, point.y * scale_)
-
-
-## Perspective projection function
-#func perspective_project(point: Vector3) -> Vector2:
-	#var m = DenseMatrix.zero(4)
-	#m.set_element(0, 0, n)
-	#m.set_element(1, 1, n)
-	#m.set_element(2, 2, f+n)
-	#m.set_element(2, 3, -f*n)
-	#m.set_element(3, 2, 1)
-	#var v = DenseMatrix.from_packed_array([point.x, point.y, point.z, 1], 4, 1)
-	#var res = m.multiply_dense(v)
-	#
-	#return Vector2(res.get_element(0, 0), res.get_element(0, 1))
-
+var axis = F.Axis.new()
 var frame_count = 0
 
 func _ready() -> void:
+	tetrahedron.translate(200, 200, 200)
+	cube.translate(200, 200, 200)
+	axis.translate(200, 200, 200)
 	Engine.max_fps = 20
+
 func _process(delta: float) -> void:
 	if (frame_count > 100):
 		return
 	frame_count += 1
-	tetrahedron.translate(0, 0, 1)
+	tetrahedron.translate(1, 1, 1)
+	cube.translate(-1, -1, -1)
 	queue_redraw()
+
+func draw_object(obj: F.Spatial):
+	for edge in obj.edges:
+		var p1: F.Point = obj.points[edge.x]
+		var p2: F.Point = obj.points[edge.y]
+		
+		var axonometric_matrix = F.AffineMatrices.get_perspective_matrix(-1000)
+		
+		p1.apply_matrix(axonometric_matrix)
+		p2.apply_matrix(axonometric_matrix)
+		draw_line(p1.get_vec2d(), p2.get_vec2d(), Color.RED, 0.5, true)
+
+func draw_axis(axis: F.Axis):
+	var perspective_matrix = F.AffineMatrices.get_perspective_matrix(-1000)
+	var p1: F.Point = axis.points[axis.edges[0].x]
+	var p2: F.Point = axis.points[axis.edges[0].y]
+	p1.apply_matrix(perspective_matrix)
+	p2.apply_matrix(perspective_matrix)
+	draw_line(p1.get_vec2d(), p2.get_vec2d(), Color.RED, 0.5, true)
+	
+	p1 = axis.points[axis.edges[1].x]
+	p2 = axis.points[axis.edges[1].y]
+	p1.apply_matrix(perspective_matrix)
+	p2.apply_matrix(perspective_matrix)
+	draw_line(p1.get_vec2d(), p2.get_vec2d(), Color.GREEN, 0.5, true)
+	
+	p1 = axis.points[axis.edges[2].x]
+	p2 = axis.points[axis.edges[2].y]
+	p1.apply_matrix(perspective_matrix)
+	p2.apply_matrix(perspective_matrix)
+	draw_line(p1.get_vec2d(), p2.get_vec2d(), Color.BLUE, 0.5, true)
+
 	
 func _draw():
-	# Drawing the cube in 2D space with perspective projection
-	for edge in tetrahedron.edges:
-		var p1 = tetrahedron.points[edge.x]
-		var p2 = tetrahedron.points[edge.y]
-		
-		# Apply perspective projection to 2D
-		var projected_p1 = perspective_project(p1)
-		var projected_p2 = perspective_project(p2)
-		
-		#print(projected_p1, projected_p2)
-		# Draw the edges (replace with actual drawing code)
-		draw_line(projected_p1, projected_p2, Color.RED)
+	draw_object(tetrahedron)
+	draw_object(cube)
+	#draw_axis(axis)
+	
