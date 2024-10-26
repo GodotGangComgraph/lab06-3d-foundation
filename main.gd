a7907dc0646934ec13e9bf0ce4bcfb847dd1946f
Владислав Крылов <krylov@sfedu.ru>
2024-10-26 16:19:32 +0300

    update affine transform interface

--- main.gd
+++ main.gd
@@ -7,6 +7,21 @@ var tetrahedron = F.Tetrahedron.new()
 var axis = F.Axis.new()
 var frame_count = 0
 
+## Translation values
+@onready var dx: LineEdit = $HBox/MarginContainer/Menu/Translate/dx
+@onready var dy: LineEdit = $HBox/MarginContainer/Menu/Translate/dy
+@onready var dz: LineEdit = $HBox/MarginContainer/Menu/Translate/dz
+
+## Rotation values (don't forget deg_to_rad)
+@onready var ox: LineEdit = $HBox/MarginContainer/Menu/Rotate/ox
+@onready var oy: LineEdit = $HBox/MarginContainer/Menu/Rotate/oy
+@onready var oz: LineEdit = $HBox/MarginContainer/Menu/Rotate/oz
+
+## Scale values
+@onready var mx: LineEdit = $HBox/MarginContainer/Menu/Scale/mx
+@onready var my: LineEdit = $HBox/MarginContainer/Menu/Scale/my
+@onready var mz: LineEdit = $HBox/MarginContainer/Menu/Scale/mz
+
 func _ready() -> void:
 	tetrahedron.translate(200, 200, 200)
 	cube.translate(200, 200, 200)
@@ -14,6 +29,7 @@ func _ready() -> void:
 	Engine.max_fps = 20
 
 func _process(delta: float) -> void:
+	return
 	if (frame_count > 100):
 		return
 	frame_count += 1
@@ -54,7 +70,10 @@ func draw_axis(axis: F.Axis):
 
 	
 func _draw():
-	draw_object(tetrahedron)
+	#draw_object(tetrahedron)
 	draw_object(cube)
-	#draw_axis(axis)
-	
+
+func _on_apply_pressed() -> void:
+	var translate_matrix = F.AffineMatrices.get_translation_matrix(float(dx.text),float(dy.text),float(dz.text))
+	cube.apply_matrix(translate_matrix)
+	queue_redraw()
